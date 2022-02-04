66ac78c22f63a82a58781d6c5a5dd898faaf943d
SobolevicDaniell <sobolevdan99@yandex.ru>
2022-02-04 15:05:08 +0300

    MainScript

--- Assets/Scripts/MainScript.cs
+++ Assets/Scripts/MainScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class butt : MonoBehaviour
+public class MainSript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -14,16 +14,5 @@ public class butt : MonoBehaviour
     void Update()
     {
         
-    }
-
-    void Something()
-    {
-        // something
-    }
-
-    void Timer()
-    {
-        int timer = 9;
-        timer++;
     }
 }
