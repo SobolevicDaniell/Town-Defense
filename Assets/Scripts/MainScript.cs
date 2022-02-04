6823992fba69a0f6bcaa3a3f5556f7cc1ea68acc
SobolevicDaniell <sobolevdan99@yandex.ru>
2022-02-04 15:36:38 +0300

    add timer

--- Assets/Scripts/MainScript.cs
+++ Assets/Scripts/MainScript.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class MainSript : MonoBehaviour
+public class MainScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Image WheatsImage;
+    public Text WheatsText;
+
+
+
+
+
+    public int Wheats = 5;
+    public int Farmers = 1;
+    public float WheatsTime = 5;
+
+
+
+
+    float WheatsTimer;
+
     void Start()
     {
-        
+        WheatsTimer = WheatsTime;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        WheatsGenerator();
+    }
+
+
+    void WheatsGenerator()
+    {
+        WheatsTimer -= Time.deltaTime;
+        WheatsImage.fillAmount = WheatsTimer / WheatsTime;
+        if (WheatsTimer <= 0)
+        {
+            Wheats += Farmers;
+            WheatsTimer = WheatsTime;
+        }
+        WheatsText.text = Mathf.Round(Wheats).ToString();
     }
 }
