e20e4fb75fbb9f65d2dc4b3dfa64155e45830c06
BuildTools <likvidoff@gmail.com>
2022-02-09 01:31:52 +0300

    добавил таймер потребления и пофиксил два других таймера

--- Assets/Scripts/MainScript.cs
+++ Assets/Scripts/MainScript.cs
@@ -11,45 +11,58 @@ public class MainScript : MonoBehaviour
 
     public Image FarmersImage;
     public Text FarmersText;
+    public Button FarmersButton;
 
     public Image WarriorsImage;
     public Text WarriorsText;
+    public Button WarriorsButton;
 
     public Image WaveImage;
     public Text WaveText;
     public Text EnemiesText;
     public Text WaveNumberText;
+    public Image ConsumptionImage;
 
 
-
+    // Начальные настройки
     public int Wheats = 5;
     public int Farmers = 1;
     public int Warriors = 0;
     public int Enemies = 1;
+    public int FarmerConsumption = 1;
+    public int WarriorConsumption = 2;
 
+    // Цены
     public int FarmerCost = 2;
     public int WarriorsCost = 5;
 
+    // Профиты
     public int FarmersProfit = 1;
-
+    
+    // Здоровье
     public int FarmersLives = 1;
     public int WarriorsLives = 3;
     public int EnemiesLives = 3;
 
+    // Время
     public float WheatsTime = 5;
-    public float FarmersTime = 5;
+    public float FarmersTime = 2;
     public float WarriorsTime = 10;
-    public float WaveTime = 60;
+    public float WaveTime = 180;
+    public float ConsumptionTime = 30;
 
-
-
+    // Инты
     int WaveNumber = 1;
+    int FarmersQueue;
+    int WarriorsQueue;
+    int WheatsToConsumption;
 
-
+    // Флоаты
     float WheatsTimer;
     float FarmersTimer;
     float WarriorsTimer;
     float WaveTimer;
+    float ConsumptionTimer;
 
     float TotalDamage;
 
@@ -57,17 +70,24 @@ public class MainScript : MonoBehaviour
     {
         WheatsTimer = WheatsTime;
         WaveTimer = WaveTime;
+        ConsumptionTimer = ConsumptionTime;
+
+        FarmersTimer = FarmersTime;
+        WarriorsTimer = WarriorsTime;
 
         FarmersText.text = Mathf.Round(Farmers).ToString();
         WarriorsText.text = Mathf.Round(Warriors).ToString();
         WaveNumberText.text = $"Wave №{WaveNumber}";
-        EnemiesText.text = $"Enemies: {Enemies}";
+        EnemiesText.text = $"Enemies next round: {Enemies}";
     }
 
     void Update()
     {
         WheatsGenerator();
         Wave();
+        Consumption();
+        FarmersGenerator();
+        WarriorsGenerator();
     }
 
 
@@ -83,44 +103,56 @@ public class MainScript : MonoBehaviour
         WheatsText.text = Mathf.Round(Wheats).ToString();
     }
 
+    public void FarmersAddButton() { Wheats -= FarmerCost; FarmersQueue++; }
     public void FarmersGenerator()
     {
-        if (Wheats >= FarmerCost)
+        if (FarmersQueue > 0)
         {
-            Wheats = Wheats - FarmerCost;
-            FarmersText.text = Mathf.Round(Farmers).ToString();
-
-            // ошибка тут ->
-            FarmersTimer = FarmersTime;
-            while (FarmersTimer > 0)
+            FarmersTimer -= Time.deltaTime;
+            FarmersImage.fillAmount = FarmersTimer / FarmersTime;
+            if (FarmersTimer <= 0)
             {
-                FarmersTimer -= Time.deltaTime;
-                FarmersImage.fillAmount = FarmersTimer / FarmersTime;
+                FarmersQueue--;
+                Farmers++;
+                FarmersTimer = FarmersTime;
+                FarmersText.text = Mathf.Round(Farmers).ToString();
             }
-            Farmers++;
-            FarmersText.text = Mathf.Round(Farmers).ToString();
+        }
+
+        if (Wheats < FarmerCost) // Проверка на взможность покупки фермеров
+        { 
+            FarmersButton.interactable = false; 
+        } else { 
+            FarmersButton.interactable = true; 
         }
     }
 
-
+    public void WarriorsAddButton() { Wheats -= WarriorsCost; WarriorsQueue++; }
     public void WarriorsGenerator()
     {
-        if (Wheats >= WarriorsCost)
+        if (WarriorsQueue > 0)
         {
-            Wheats = Wheats - WarriorsCost;
-            WarriorsText.text = Mathf.Round(Warriors).ToString();
-
-            // ошибка тут ->
-            WarriorsTimer = WarriorsTime;
-            while (WarriorsTimer > 0)
+            WarriorsTimer -= Time.deltaTime;
+            WarriorsImage.fillAmount = WarriorsTimer / WarriorsTime;
+            if (WarriorsTimer <= 0)
             {
-                WarriorsTimer -= Time.deltaTime;
-                WarriorsImage.fillAmount = WarriorsTimer / WarriorsTime;
+                WarriorsQueue--;
+                Warriors++;
+                WarriorsTimer = WarriorsTime;
+                WarriorsText.text = Mathf.Round(Warriors).ToString();
             }
-            Warriors++;
-            WarriorsText.text = Mathf.Round(Warriors).ToString();
+        }
+
+        if (Wheats < WarriorsCost) // Проверка на взможность покупки воинов
+        {
+            WarriorsButton.interactable = false;
+        }
+        else
+        {
+            WarriorsButton.interactable = true;
         }
     }
+
     void Wave()
     {
         WaveTimer -= Time.deltaTime;
@@ -157,7 +189,24 @@ public class MainScript : MonoBehaviour
             
             WaveTimer = WaveTime;
         }
-        
     }
+    
+    void Consumption()
+    {
+        ConsumptionTimer -= Time.deltaTime;
+        ConsumptionImage.fillAmount = ConsumptionTimer / ConsumptionTime;
 
+        if (ConsumptionTimer <= 0)
+        {
+            WheatsToConsumption = Warriors * WarriorConsumption + Farmers * FarmerConsumption;
+            while (WheatsToConsumption > 0)
+            {
+                if (Wheats > 0) { Wheats--; WheatsToConsumption -= 1; }
+                else if (Warriors > 0) { WheatsToConsumption -= WarriorConsumption; Warriors--; }
+                else if (Farmers > 0) { WheatsToConsumption -= FarmerConsumption; Farmers--; }
+                else { /* Loosing script */ }
+            }
+            ConsumptionTimer = ConsumptionTime;
+        }
+    }
 }
